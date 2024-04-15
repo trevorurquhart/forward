8496476d21c43b98ccc23c0b50ad8dce46be7657
Trevor Urquhart <trevor.urquhart@gmail.com>
2024-04-15 21:41:32 +1200

    test - assert balance is adjusted in destination

--- tests/anchor.ts
+++ tests/anchor.ts
@@ -1,76 +1,81 @@
 import * as anchor from "@coral-xyz/anchor";
 import * as web3 from "@solana/web3.js";
+import {expect} from "chai";
 // import {
 //   createMint,
 //   createAssociatedTokenAccount,
 //   mintTo,
 //   TOKEN_PROGRAM_ID,
 // } from "@solana/spl-token";
-import type { Forward } from "../target/types/forward";
+import type {Forward} from "../target/types/forward";
 
 describe("Test forward", () => {
-  // Configure the client to use the local cluster
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster
+    anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Forward as anchor.Program<Forward>;
-  
-  it("test forwarding SOL", async () => {
+    const program = anchor.workspace.Forward as anchor.Program<Forward>;
 
-    const forwardId = 104;
-    const destinationKp = new web3.Keypair();
-    const quarantineKp = new web3.Keypair();
+    it("test forwarding SOL", async () => {
 
-    const [forward] = web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("forward"), destinationKp.publicKey.toBuffer(), new anchor.BN(forwardId).toArrayLike(Buffer, "le", 8)],
-      program.programId
-    );
+        const forwardId = 104;
+        const destinationKp = new web3.Keypair();
+        const quarantineKp = new web3.Keypair();
 
-    // console.log("Calcuated pda as: ", forward.toString());
+        const [forward] = web3.PublicKey.findProgramAddressSync(
+            [Buffer.from("forward"), destinationKp.publicKey.toBuffer(), new anchor.BN(forwardId).toArrayLike(Buffer, "le", 8)],
+            program.programId
+        );
 
-    const txInitialiase = await program.methods
-      .initialize(new anchor.BN(forwardId), quarantineKp.publicKey)
-      .accounts({
-        forward: forward,
-        destination: destinationKp.publicKey,
-      })
-      .signers([program.provider.wallet.payer])
-      .rpc();
+        //Create the forward
+        const txInitialise = await program.methods
+            .initialize(new anchor.BN(forwardId), quarantineKp.publicKey)
+            .accounts({
+                forward: forward,
+                destination: destinationKp.publicKey,
+            })
+            .signers([program.provider.wallet.payer])
+            .rpc();
 
-    // console.log(`https://explorer.solana.com/tx/${txInitialiase}?cluster=devnet`);
-    await program.provider.connection.confirmTransaction(txInitialiase, "finalized");
+        // console.log(`https://explorer.solana.com/tx/${txInitialiase}?cluster=devnet`);
+        await program.provider.connection.confirmTransaction(txInitialise, "finalized");
 
+        //Deposit to the forward
+        let depositAmount = web3.LAMPORTS_PER_SOL / 100;
+        let blockhash = await program.provider.connection.getLatestBlockhash().then(res => res.blockhash);
+        const instructions = [web3.SystemProgram.transfer({
+            fromPubkey: program.provider.publicKey,
+            toPubkey: forward,
+            lamports: depositAmount
+        })];
 
-    let blockhash = await program.provider.connection.getLatestBlockhash().then(res => res.blockhash);
-    const instructions = [web3.SystemProgram.transfer({
-        fromPubkey: program.provider.publicKey,
-        toPubkey: forward,
-        lamports: web3.LAMPORTS_PER_SOL / 100
-      })];
+        const messageV0 = new web3.TransactionMessage({
+            payerKey: program.provider.publicKey,
+            recentBlockhash: blockhash,
+            instructions
+        }).compileToV0Message();
+        const transaction = new web3.VersionedTransaction(messageV0);
+        transaction.sign([program.provider.wallet.payer]);
+        const txDeposit = await program.provider.connection.sendTransaction(transaction);
 
-    const messageV0 = new web3.TransactionMessage({
-      payerKey: program.provider.publicKey,
-      recentBlockhash: blockhash,
-      instructions
-    }).compileToV0Message();
-    const transaction = new web3.VersionedTransaction(messageV0);
-    transaction.sign([program.provider.wallet.payer]);
-    const txDeposit = await program.provider.connection.sendTransaction(transaction);
+        // console.log(`https://explorer.solana.com/tx/${txDeposit}?cluster=devnet`);
+        await program.provider.connection.confirmTransaction(txDeposit, "finalized");
 
-    // console.log(`https://explorer.solana.com/tx/${txDeposit}?cluster=devnet`);
-    await program.provider.connection.confirmTransaction(txDeposit, "finalized");
+        //Execute forward
+        const txExecute = await program.methods
+            .forwardSol()
+            .accounts({
+                forward: forward,
+                destination: destinationKp.publicKey
+            })
+            .signers([program.provider.wallet.payer])
+            .rpc();
 
-    const txExecute = await program.methods
-        .forwardSol()
-        .accounts({
-          forward: forward,
-          destination: destinationKp.publicKey
-        })
-        .signers([program.provider.wallet.payer])
-        .rpc();
+        // console.log(`https://explorer.solana.com/tx/${txExecute}?cluster=devnet`);
+        await program.provider.connection.confirmTransaction(txExecute, "finalized");
 
-    // console.log(`https://explorer.solana.com/tx/${txExecute}?cluster=devnet`);
-    await program.provider.connection.confirmTransaction(txExecute, "finalized");
+        const balance = await program.provider.connection.getBalance(destinationKp.publicKey);
+        expect(balance).to.equal(depositAmount);
 
-  });
+    });
 
 });
